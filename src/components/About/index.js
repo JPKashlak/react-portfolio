730fccd03e30f78b094939e3d3c94f4f4b7fd527
jpkashlak <jpkashlak@gmail.com>
2021-05-06 20:22:55 -0500

    set up files and began About & Contact

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Jacob Pearson Kashlak</h1>
+      <img src={require('../../assets/images/portrait.jpeg')} alt="programmer's portrait"/>
+    </section>
+  );
+}
+
+export default About;
