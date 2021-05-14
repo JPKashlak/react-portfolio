42dd893b93084929024f21b76f704c9e9b22988b
jpkashlak <jpkashlak@gmail.com>
2021-05-14 11:47:32 -0500

    fixed download resume link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,7 +4,7 @@ function Resume() {
     return(
         <section id="resume">
             <h2><span>Resume</span></h2> 
-            <a href="../../assets/documents/Resume_Kashlak.pdf" download>Download Resume</a>
+            <a href="https://drive.google.com/file/d/1DWUWEpJpsNqp9xgdIT4xgvD-i1kBT-kv/view?usp=sharing" download targe="_blank">Download Resume</a>
         </section>
     )
 }
